58f5abe34bac9bd609642aa0f11f60eed827df92
Kossyak <68551616+kossyak@users.noreply.github.com>
2023-01-08 19:15:24 +0400

    Update csm.js

--- csm.js
+++ csm.js
@@ -1,29 +1,50 @@
 export default {
   context: [],
   current: [],
+  actions: {},
   state: {},
   events: {},
   create(entry) {
     this.context = this.value(entry.context)
-    this.state = entry.state
+    this.actions = entry.actions
+    this.level = entry.actions
     this.emit('_create', {...entry})
     this.context.forEach(key => this.emit(key, this.context))
     this.update()
   },
   destroy() {
     this.context = []
-    this.state = {}
+    this.actions = {}
+    this.level = {}
     this.emit('_destroy', this.context)
   },
-  off() {
-    this.events = {}
+  off(key) {
+    if (key) {
+      delete this.events[key]
+    } else this.events = {}
   },
   value(v) {
     if (!v) return []
     return Array.isArray(v) ? v : [v]
   },
+  deliver(path) {
+    let i, target = {...this.actions}
+    for (i = 0; i < path.length - 1; i++) target = target[path[i]]
+    return target[path[i]]
+  },
+  to(action) {
+    if (action.to === '/') {
+      this.level = this.actions
+    } else {
+      const path = action.to.slice(1).split('/')
+      this.level = this.deliver(path).actions
+    }
+  },
   async action(key, data) {
-    const action = this.state[key]
+    const action = {...this.level[key]}
+    if (action.to) {
+      this.to(action)
+    } else if (action.actions) this.level = action.actions
     if (action) {
       if (!this.context.includes(key)) this.context.push(key)
       this.push(action)
@@ -53,26 +74,28 @@ export default {
       this.context = this.context.filter(c => !excludeSet.has(c))
     }
   },
-  tabu(action) {
-    if (action.tabu) {
-      return this.value(action.tabu).some((a) => this.context.includes(a))
+  match(arr) {
+    let count = 0
+    for (const el of arr) {
+      if (this.context.includes(el)) count++
     }
+    return arr.length === count
+  },
+  tabu(target) {
+    const arr = this.value(target)
+    return arr.length ? this.match(arr) : false
   },
   update(trigger) {
     const actions = []
-    for (const [key, action] of Object.entries(this.state)) {
-      if (!this.tabu(action)) {
-        let count = 0
+    for (const [key, action] of Object.entries(this.level)) {
+      if (!this.tabu(action.tabu)) {
         const permit = [...this.value(action.pop), ...this.value(action.permit)]
-        if (permit.length) {
-          for (const p of permit) {
-            if (this.context.includes(p)) count++
-          }
-          if (permit.length === count) actions.push(key)
-        } else actions.push(key)
+        if (trigger && this.level !== this.actions) permit.push(trigger)
+        if (!permit.length) return console.error('permit not found for ' + key)
+        this.match(permit) && actions.push(key)
       }
     }
-    this.emit('_change', {actions, trigger, context: this.context})
+    this.emit('_change', {level: this.level, actions, trigger, context: this.context})
     if (actions.toString() !== this.current.toString()) {
       this.current = [...actions]
     }
